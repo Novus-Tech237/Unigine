bd5b02949e1a4037f49ff2435e024e458bd3035c
Novus-Tech237 <novustechnologies7@gmail.com>
2024-05-17 17:14:38 +0100

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   prisma/migrations/20240404150522_unigine_database_migration/migration.sql 	new file:   prisma/migrations/20240404150824_unigine_database_migration/migration.sql 	new file:   query

---
+++ prisma/migrations/20240404150522_unigine_database_migration/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `block` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `follow` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `stream` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `block`;
+
+-- DropTable
+DROP TABLE `follow`;
+
+-- DropTable
+DROP TABLE `stream`;
+
+-- DropTable
+DROP TABLE `user`;
